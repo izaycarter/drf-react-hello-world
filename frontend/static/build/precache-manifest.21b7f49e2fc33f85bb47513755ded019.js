47d2c3efc17de806266dfd7bdaa06e17809cf817
Isaiah Carter <izay.carter0509@gmail.com>
2019-10-21 13:58:43 -0400

    test again

---
+++ frontend/static/build/precache-manifest.21b7f49e2fc33f85bb47513755ded019.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7782cd61769731b016fde513b35935",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3decd9fac43c6756da26",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0c5f556b58ee8825dca6",
+    "url": "/static/js/2.0760e9c0.chunk.js"
+  },
+  {
+    "revision": "3decd9fac43c6756da26",
+    "url": "/static/js/main.3bdff45f.chunk.js"
+  },
+  {
+    "revision": "71dcedd2f2bbe7dcbd91",
+    "url": "/static/js/runtime-main.107b9db7.js"
+  }
+]);
